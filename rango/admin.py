e1f29341a63c30c7781b155eb77e048f28180675
Yao Du <2359451d@student.gla.ac.uk>
2020-01-31 03:09:28 +0000

    finished chapter6 fixed with url format in script

--- rango/admin.py
+++ rango/admin.py
@@ -5,7 +5,14 @@ from rango.models import Category, Page
 
 class PageAdmin(admin.ModelAdmin):
     # customising the Page page
-    list_display = ('title', 'category','url')
+    list_display = ('title', 'category','url','views')
 
-admin.site.register(Category)
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    # automatically populates the slug fields when type name 
+    # specific variable in the admin to automate the process for us
+    prepopulated_fields = {'slug':('name',)}
+    list_display = ('name','views','likes','slug')
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
